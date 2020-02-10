c2ffcdcc3e34bb74a84fc6982619bedda4b230ed
Ben <bennie.ben095@gmail.com>
2020-02-09 22:19:04 -0800

    #1 ~ #2 Done

---
+++ extractor/extractor.go
@@ -0,0 +1,91 @@
+package extractor
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var baseURL = "C://Users/exit2/Desktop/WorkSpace/tabula-spring_2019.csv"
+
+// Course Structure
+type Course struct {
+	Name      string
+	Professor string
+	Grades    grade
+	Students  string
+}
+
+type grade struct {
+	A  string
+	B  string
+	C  string
+	D  string
+	F  string
+	NP string
+	P  string
+	W  string
+}
+
+// Extract Files
+func Extract() []byte {
+	csvFile, err := os.Open(baseURL)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer csvFile.Close()
+	reader := csv.NewReader(csvFile)
+	reader.FieldsPerRecord = -1
+
+	csvData, err := reader.ReadAll()
+	results := getAllCors(csvData)
+	jsonData, err := json.Marshal(results)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return jsonData
+}
+
+func getAllCors(row [][]string) []Course {
+	var cors []Course
+
+	courseName := 2
+	profName := 3
+	colA := 4
+	colB := 5
+	colC := 6
+	colD := 7
+	colF := 8
+	colNP := 11
+	colP := 12
+	colW := 14
+	colT := 15
+
+	for i := 1; i < len(row); i++ {
+		Name := row[i][courseName]
+		Professor := row[i][profName]
+		Students := row[i][colT]
+		Grades := grade{
+			A:  row[i][colA],
+			B:  row[i][colB],
+			C:  row[i][colC],
+			D:  row[i][colD],
+			F:  row[i][colF],
+			NP: row[i][colNP],
+			P:  row[i][colP],
+			W:  row[i][colW],
+		}
+		newCourse := Course{Name, Professor, Grades, Students}
+		cors = append(cors, newCourse)
+	}
+	return cors
+}
+
+func cleanString(s string) string {
+	str := strings.Split(s, "   ")
+	newString := strings.Join(str, ",")
+	return newString
+}
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/Bundy-Mundi/profgradedist/extractor"
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	jsonData := extractor.Extract()
+	s := string(jsonData)
+	e.GET("/", func(c echo.Context) error {
+		return c.JSONPretty(http.StatusOK, s, "  ")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
